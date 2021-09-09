83de835d4979251bf7eafbb7c340bc9c3f708b56
Yuchira Athukorala <yuchira.athukorala@gmail.com>
2021-09-09 17:25:05 +0530

    learn getwd() and setwd() methods

--- first_script.R
+++ first_script.R
@@ -12,3 +12,14 @@ class(Gender)
  
 #method to get current working directory
 getwd()
+
+#change the current working directory path
+setwd("C:\\R Studio Projects\\R_Studio_Lab_1\\yja")
+
+getwd()
+setwd("C:\\R Studio Projects\\R_Studio_Lab_1")
+getwd()
+setwd("C://R Studio Projects//R_Studio_Lab_1//yja")
+getwd()
+setwd("C://R Studio Projects//R_Studio_Lab_1")
+getwd()
