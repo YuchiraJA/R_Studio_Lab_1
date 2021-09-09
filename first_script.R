96f5f508ebaf5c6593cbd674a836f6cb0a4ae06e
Yuchira Athukorala <yuchira.athukorala@gmail.com>
2021-09-09 18:58:18 +0530

    updated

--- first_script.R
+++ first_script.R
@@ -16,6 +16,7 @@ getwd()
 #change the current working directory path
 setwd("C:\\R Studio Projects\\R_Studio_Lab_1\\yja")
 
+#doing some examples
 getwd()
 setwd("C:\\R Studio Projects\\R_Studio_Lab_1")
 getwd()
@@ -23,3 +24,50 @@ setwd("C://R Studio Projects//R_Studio_Lab_1//yja")
 getwd()
 setwd("C://R Studio Projects//R_Studio_Lab_1")
 getwd()
+
+
+#method to get help tp solve a system of equations
+?solve
+
+#method to know about data frames | get infor about data frames
+help(data.frame)
+
+
+#to clear the console , we can use ctrl+l button pressing 
+
+
+
+#Operator and #Description 
+
+# + Addition 
+# - Subtraction 
+# * Multiplication 
+# / Division 
+# ^ or ** Exponentiation 
+# %% Modulus (mod) 
+# %/% Integer Division
+ 
+
+
+#Operator #Description 
+
+# < less than 
+# <= less than or equal to 
+# > greater than 
+# >= greater than or equal to 
+# == exactly equal to 
+# !=  not equal to 
+# ! NOT 
+# | OR 
+# & AND 
+# isTRUE(x) test if x is TRUE
+
+
+
+#Creating New Variables#
+
+#The operators "<-" and "=" assign into the environment in which they are evaluated. The operator <- can be used anywhere, whereas the operator = is only 
+#allowed at the top level (e.g., in the complete expression typed at the command prompt) or as one of the sub expressions in a braced list of expressions.
+
+#Operator "<<-" is normally only used in functions, to access variables in the global environment, instead of the local environment.
+
